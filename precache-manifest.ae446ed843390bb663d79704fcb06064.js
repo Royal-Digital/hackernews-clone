01deb9756898390e41b967bbcdfd24312711c925
Precious Jahlom Agboado <jahlom@mpharma.com>
2020-05-02 06:59:49 +0000

    Updates

---
+++ precache-manifest.ae446ed843390bb663d79704fcb06064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96a7c2e4141a5aa660b2f5cec5f163c",
+    "url": "/hackernews-clone/index.html"
+  },
+  {
+    "revision": "8e73f59fb2cc69bd4841",
+    "url": "/hackernews-clone/static/css/main.b0bb0bb0.chunk.css"
+  },
+  {
+    "revision": "b836d96d75603fc89974",
+    "url": "/hackernews-clone/static/js/2.1a06417c.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/hackernews-clone/static/js/2.1a06417c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e73f59fb2cc69bd4841",
+    "url": "/hackernews-clone/static/js/main.b7eb2c8c.chunk.js"
+  },
+  {
+    "revision": "e669d2bf4cef99f7915c",
+    "url": "/hackernews-clone/static/js/runtime-main.f1727690.js"
+  },
+  {
+    "revision": "8b4518ed224402f7b4bb8cc7eecb9810",
+    "url": "/hackernews-clone/static/media/SearchIcon.8b4518ed.svg"
+  }
+]);
